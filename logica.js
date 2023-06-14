3dcb8fdb70f0abd4fa3646e86402835bf7b8d92d
Agus2020 <aagustin067@gmail.com>
2023-06-13 22:04:34 -0300

    Lista de estudiantes terminada

--- logica.js
+++ logica.js
@@ -1,81 +1,88 @@
-console.table(productos)
-const carrito = []
-let contenedorProds = document.getElementById('misprods');
+// Mostramos por consola una tabla de estudiantes para verificar todo OK
+console.table(students)
+// Declaramos las variables que vamos a utilizar
+let contenedorAlumnos = document.getElementById('misAlumnos');
 let contenedorTotal = document.getElementById('total');
 let contenedorNombre = document.getElementById('nombre');
 let contenedorPassword = document.getElementById('password');
-let contenedorMostrarProductos = document.getElementById('buttonMostrarProductos');
-let contenedorButtonTable = document.getElementsByClassName('btnTable')
-let precioMax=100000;
-console.log('sdasd'+contenedorMostrarProductos)
 
-function filtrarPrecio(precio){
-    const filtrados = productos.filter((prod)=>prod.precio <= precio);
-    return filtrados;
+
+// Por defecto llamamos a la funcion para siempre mostrar los estudiantes al comienzo
+renderizarAlumnos(students)
+
+
+
+
+
+// Esta funcion renderiza los alumnos por medio de un for of 
+
+function renderizarAlumnos(lista){
+    contenedorAlumnos.innerHTML = '';
+    for(const alumno of lista){
+        contenedorAlumnos.innerHTML += `
+            <td>${alumno.id}</td>
+            <td>${alumno.name}</td>
+            <td>${alumno.age}</td>
+            <td>${alumno.note}</td>
+            <td>${alumno.genre}</td>
+        `
+    }
+    
 }
 
 
-function filtrarPorPrecioButton(){
-        contenedorProds.innerHTML=''
-        precioMax = parseFloat(prompt('Ingresa el precio maximo que se puede abonar'));
-        if(isNaN(precioMax) || precioMax < 0){
+
+// Esta funcion Vuelve a renderizar los alumnos
+function mostrarAlumnos(){
+    renderizarAlumnos(students)
+}
+
+
+// Esta funcion filtrara por nota maxima
+function filtrarPorNota(note){
+    const filtrados = students.filter((item)=>item.note <= note);
+    return filtrados;
+}
+
+// Esta funcion nos pide por medio de un prompt, que carguemos la nota maxima y hara los filtros
+function filtrarPorNotaButton(){
+    contenedorAlumnos.innerHTML=''
+        notaMax = parseFloat(prompt('Ingresa la nota maxima'));
+        if(isNaN(notaMax) || notaMax < 0){
             alert('Ingrese un numero valido')
         }else{
-            const prodsFiltrados = filtrarPrecio(precioMax);
-            console.table(prodsFiltrados);
-            renderizarProductos(prodsFiltrados)
+            const alumnosFiltrados = filtrarPorNota(notaMax);
+            console.table(alumnosFiltrados);
+            renderizarAlumnos(alumnosFiltrados)
     
         }
 }
 
+//Formulario
 
+// Esta funcion borra los campos del formulario
 function borrarCampos(){
     contenedorNombre.value=''
     contenedorPassword.value='';
 }
 
-contenedorButtonTable.onmouseover = () => {
-    contenedorButtonTable.classList.replace('btn-primary','btn-warning')
-}
-
-
-function renderizarProductos(listaProds){
-    contenedorProds.innerHTML = ''
-    for(const prod of listaProds){
-        contenedorProds.innerHTML += `
-            <td>${prod.id}</td>
-            <td>${prod.nombre}</td>
-            <td>${prod.precio}</td>
-            <td><button id=${prod.id} class="btnTable">+</button></td>
-        `
-    }
-    
-}
-console.log(contenedorNombre)
-
+// Esta funcion verifica que los inputs contengan almenos un valor para poder enviarse
+// Una vez que termina se vacian y mandan un alert saludando al usuario
 
 function enviarDatos(){
     if(contenedorPassword.value.trim() ==='' || contenedorNombre.value.trim() ===''){
         alert('Error cargue los campos')
     }else{
         alert(`Bienvenido ${contenedorNombre.value} su contraseña es ${contenedorPassword.value}`)
-        borrarCampos()   
+        localStorage.setItem('nombre',contenedorNombre.value) // Guardamos en el localStorage el nombre de usuario
+        localStorage.setItem('password',contenedorPassword.value) // Guardamos en el localStorage el password de usuario
+        borrarCampos()   // Borramos los campos una vez enviados
     }
 
 }
 
-for(const boton of contenedorButtonTable){
-    boton.addEventListener('click',()=>{
-        console.log(agregarProdsCarrito)
-        const agregarProdsCarrito = productos.find((prod)=>prod.id === boton.id)
-        carrito.push(agregarProdsCarrito)
-    })
-}
 
 
+// Guardamos en el localStorage los datos del usuario
 
-function mostrarProductos(){
-    renderizarProductos(productos)
-}
 
-renderizarProductos(productos)
